aa4b1f1a510c5d1545a4d6a23c0f80b7cf578db2
Dev-Joshua <Joshua_dup@hotmail.com>
2023-02-17 16:49:15 -0500

    empezamos a trabajar con el posicionamiento de los elementos en el mapa

--- main.js
+++ main.js
@@ -39,28 +39,46 @@ function startGame() {
   game.font = elementsSize + "px Verdana";
   game.textAlign = "end";
 
-  const map = maps[2];
-  //Creo un array(donde el inicio y el final de c/elemento es cuando haya un salto de linea ), selecciono el mapa[0] le quito los espacios en blanco .trim() y
+  const map = maps[0];
+  //Creo un array de filas(donde el inicio y el final de c/elemento es cuando haya un salto de linea), selecciono el mapa le quito los espacios en blanco .trim(limpiar string)
   const mapRows = map.trim().split("\n");
+  //Creo un array a partir de otro array(bidimensional). Cada fila va a ser otro array donde c/elemento son las distintas columnas
   const mapRowCols = mapRows.map((row) => row.trim().split(""));
   console.log({ map, mapRows, mapRowCols });
 
-  //Con un ciclo anidado agregamos los emoji a cada elementSize(filas[i] y columnas[j]) de la grilla. => Map[row["col"]]
-  for (let i = 1; i <= 10; i++) {
-    // console.log(i);
-    for (let j = 1; j <= 10; j++) {
-      //En cada iteracion renderizamos filtext
-      // game.fillText(emojis["X"], elementsSize * i, elementsSize * j);
-      // console.log(j);
-      // game.fillText(mapRowCols[i][j], elementsSize * i, elementsSize * j);
-      //Le enviamos el mapRowCols al objeto de emojis para que tome el elemento que concuerde con estas ubicaciones(i,j-> row,col)
-      game.fillText(
-        emojis[mapRowCols[i - 1][j - 1]],
-        elementsSize * i,
-        elementsSize * j
-      );
-    }
-  }
+  //minimizo el codigo de for a un forEach. -> Este metodo permite saber cual es el elemento que estoy recorriendo y saber su indice
+  //recibe dos parametros(fila[array de columnas], indice de cada fila) y asi mismo con (col,colI)
+  mapRowCols.forEach((row, rowI) => {
+    row.forEach((col, colI) => {
+      //Obtengo el emoji(por la letra) por su propiedad del objeto emojis
+      const emoji = emojis[col];
+      //Calculo la coordenada en x,y => LA posicion en x sera(multiplicar el tamaño que debe tener nuestros elementos * indice(rowi) + 1.)
+      const posX = elementsSize * (rowI + 1);
+      const posY = elementsSize * (colI + 1);
+      //Le asigno a la funcion para renderizar cualquier mapa segun las posiciones, segun el array maps.
+      game.fillText(emoji, posX, posY);
+      // console.log(emojis[col]);
+      // console.log({ row, col });
+      console.log({ row, rowI, col, colI });
+    });
+  });
+
+  // //Con un ciclo anidado agregamos los emoji a cada elementSize(filas[i] y columnas[j]) de la grilla. => Map[row["col"]]
+  // for (let i = 1; i <= 10; i++) {
+  //   // console.log(i);
+  //   for (let j = 1; j <= 10; j++) {
+  //     //En cada iteracion renderizamos los elementos llenando el mapa
+
+  //     // game.fillText(mapRowCols[i][j], elementsSize * i, elementsSize * j);
+  //     //Le enviamos el mapRowCols al objeto de emojis para que tome el elemento que concuerde con estas ubicaciones(i,j-> row,col)
+  //     game.fillText(
+  //       emojis[mapRowCols[i - 1][j - 1]],
+  //       elementsSize * i,
+  //       elementsSize * j
+  //     );
+  //     // console.log(j);
+  //   }
+  // }
   /*
   game.font = "14px Verdana";
   game.fillStyle = "purple";
@@ -68,4 +86,13 @@ function startGame() {
   game.fillText("VideoGame", 10, 30);*/
 }
 
-//Cada vez que quiera moverme en el canvas de izquierda a derecha sera el eje x, para movernos de arriba hacia abajo sera en el eje y
+/*
+  -> Cada vez que quiera moverme en el canvas de izquierda a derecha sera el eje x, para movernos de arriba hacia abajo sera en el eje y
+  -> Para poder llenar el mapa de canvas con cada uno de los emojis en una grilla de 10x10 se crea un arry bidimensional a partir de un array. 
+     Asi obtenemos las filas y columnas, como resultado tendremos un array donde el mapa es un array de filas y cada fila es un array de columnas.
+  -> En el ciclo for anidado creo la logica para a partir de los indices(que nos da cada uno de los ciclos for) acceder a la fila y columna correspondiente
+     al elemento que estamos renderizando en el canvas.
+
+  -> Utilizo el metodo forEach para recorrer el array
+  -> Utilizo el metodo map para crear otro array a partir de recorrer un array
+*/
